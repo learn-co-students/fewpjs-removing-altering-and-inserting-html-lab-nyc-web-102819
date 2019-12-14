0257b83cc709acbe55036667d89708e4807458ce
github username <github email address>
2019-12-14 14:48:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.getElementById("main")
+main.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerText = "Triona is the Champion"
+
+document.appendChild(newHeader)

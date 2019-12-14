740847536be19f908d4b45dec3014affd834c433
hdp2112 <hdp2112@columbia.edu>
2019-12-14 16:43:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let main = document.querySelector('#main')
+main.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Test is the champion'
